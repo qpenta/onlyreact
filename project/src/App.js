d5370858ded2461890fc12ba603926149b695651
qpenta <zzyagos1@gmail.com>
2023-06-03 02:21:34 -0300

    trabalhando com react dom

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,22 @@
+import { Link, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+
 function App() {
   return (
     <div className="container">
       <h1 className="box-degrade">
         Welcome to the mato
       </h1>
-      <button className="start-button">Start</button>
+      <Link to="/home" className="start-button">
+        Start
+      </Link>
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
--- project/src/index.js
+++ project/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import './background.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+
+ReactDOM.render(
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
 );
+
+
 
 
