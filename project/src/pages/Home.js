8a62221faf78e8f030131331aedb9b3dbefc6d21
qpenta <zzyagos1@gmail.com>
2023-06-08 23:54:04 -0300

    ..

--- project/src/pages/Home.js
+++ project/src/pages/Home.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
-            <h1>Página Home</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur recusandae perferendis repudiandae velit et consequuntur magni quos voluptate architecto quia deleniti fugiat aspernatur natus, quaerat, quis in dolore. Mollitia, optio?</p>
-        </div>
+        <body id='home'>
+            <div>
+                <h1>Welcome to the home</h1>
+            </div>
+
+        </body>
     )
 }
 
--- project/src/routes/root.jsx
+++ project/src/routes/root.jsx
@@ -1,14 +1,12 @@
 export default function Root() {
     return (
-        <>
+        <body className="root" >
             <div className="box-degrade">
                 <h1>Welcome to the mato</h1>
             </div>
             <div className="button-container">
-                <a href="/home" class="button">Lets Start</a>
+                <a href="/home" className="button">Let's Start</a>
             </div>
-
-
-        </>
+        </body>
     );
 }
