0f3fb9e877390ee4226c3d4ca38cf8cf9141d25f
qpenta <zzyagos1@gmail.com>
2023-08-28 15:44:07 -0300

    formatando pc atualizando tudo

--- projetoyt/costs/src/components/Project/ProjectForm.js
+++ projetoyt/costs/src/components/Project/ProjectForm.js
@@ -1,80 +1,78 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import styles from './ProjectForm.module.css'
-import Input from '../form/Input'
-import Select from '../form/Select'
-import SubmitButton from '../form/SubmitButton'
+import styles from "./ProjectForm.module.css";
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
 
 function ProjectForm({ handleSubmit, btnText, projectData }) {
+  const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
-    const [categories, setCategories] = useState([])
-    const [project, setProject] = useState(projectData || {})
+  useEffect(() => {
+    fetch(" https://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    useEffect(() => {
-        fetch(" https://localhost:5000/categories", {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((resp) => resp.json())
-            .then((data) => {
-                setCategories(data)
-            })
-            .catch((err) => console.log(err))
-    }, [])
+  const submit = (e) => {
+    e.prevent.default();
 
-    const submit = (e) => {
-        e.prevent.default()
+    handleSubmit(project);
+  };
 
-        handleSubmit(project)
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  }
 
-    }
+  function handleCategory(e) {
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+      },
+    });
+  }
 
-    function handleChange(e) {
-        setProject({ ...project, [e.target.name]: e.target.value })
-    }
-
-    function handleCategory(e) {
-        setProject({
-            ...project, category: {
-                id: e.target.value,
-                name: e.target.options[e.target.selectedIndex].text,
-            },
-        })
-    }
-
-
-    return (
-        <form onSubmit={submit} className={styles.form}>
-            <Input
-                type="text"
-                text="Nome do projeto:"
-                name="name"
-                placeholder='Insira o nome do projeto'
-                handleOnChange={handleChange}
-                value={project.name ? project.name : ''}
-            />
-            <Input
-                type="number"
-                text="Orçamento do projeto"
-                name="budget"
-                placeholder='Insira o orçamento total'
-                handleOnChange={handleChange}
-                value={project.budget ? project.budget : ''}
-            />
-            <div>
-                <Select name="category_id"
-                    text="Selecione uma categoria"
-                    options={categories}
-                    handleOnChange={handleCategory}
-                    value={project.category ? project.category.id : ''}
-                />
-            </div>
-            <SubmitButton text={btnText} />
-
-        </form>
-    )
+  return (
+    <form onSubmit={submit} className={styles.form}>
+      <Input
+        type="text"
+        text="Nome do projeto:"
+        name="name"
+        placeholder="Insira o nome do projeto"
+        handleOnChange={handleChange}
+        value={project.name ? project.name : ""}
+      />
+      <Input
+        type="number"
+        text="Orçamento do projeto"
+        name="budget"
+        placeholder="Insira o orçamento total"
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget : ""}
+      />
+      <div>
+        <Select
+          name="category_id"
+          text="Selecione uma categoria"
+          options={categories}
+          handleOnChange={handleCategory}
+          value={project.category ? project.category.id : ""}
+        />
+      </div>
+      <SubmitButton text={btnText} />
+    </form>
+  );
 }
 
-export default ProjectForm
+export default ProjectForm;
